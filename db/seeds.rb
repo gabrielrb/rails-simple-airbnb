768678697f7bed25fc6f610c331847e9ba7e4d78
gabriel belo <gabrielrbelo@gmail.com>
2020-08-19 16:56:02 -0300

    model, controller, index view created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+puts 'Creating restaurants...'
+
+10.times do
+  Flat.create!(name: Faker::Company.name,
+               address: Faker::Address.street_address,
+               description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+               price_per_night: rand(30..500),
+               number_of_guests: rand(1..4))
+end
+puts 'Finished!'
